634b44726f218048002cbf9e195a6241a2fdbd55
zpjune <1084013577@qq.com>
2020-04-13 11:23:44 +0800

    修改

---
+++ HouseManage/House.Service/Merchants/MerchantSvc.cs
@@ -0,0 +1,19 @@
+﻿using Data.MSSQL;
+using House.IService.Merchants;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace House.Service.Merchants
+{
+   public class MerchantSvc: IMerchantSvc
+    {
+
+        private IDataConfig _Db = null;
+        public MerchantSvc(IDataConfig dataConfig)
+        {
+            this._Db = dataConfig;
+        }
+
+    }
+}
---
+++ HouseManage/HouseManage/Controllers/Merchants/MerchantsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using House.IService.Merchants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseManage.Controllers.Merchants
+{
+    public class MerchantsController : Controller
+    {
+        private IMerchantSvc _machantSvc = null;
+        public MerchantsController(IMerchantSvc machantSvc)
+        {
+            this._machantSvc = machantSvc;
+        }
+        [AllowAnonymous]
+        public IActionResult ShopInfos()
+        {
+            return View();
+        }
+    }
+}
