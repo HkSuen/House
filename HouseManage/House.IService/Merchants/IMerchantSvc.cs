19e574ae4e961be6715f0947d7d4394cad823e36
zpjune <1084013577@qq.com>
2020-04-28 11:30:52 +0800

    修改

--- HouseManage/House.IService/Merchants/IMerchantSvc.cs
+++ HouseManage/House.IService/Merchants/IMerchantSvc.cs
@@ -1,5 +1,6 @@
 ﻿using Data.MSSQL.Model.Data;
 using House.IService.Model;
+using House.IService.Model.Enum;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,13 +11,13 @@ namespace House.IService.Merchants
     {
         Dictionary<string, object> GetBaseData(DictionaryModel model);
 
-        Dictionary<string, object> GetMerchantList(wy_houseinfo model);
-        Dictionary<string, object> GetMerchantListByPage(string FWBH, int FWSX, string SSQY, string LSFGS, int page, int sixe);
+        Dictionary<string, object> GetMerchantList(wy_houseinfo model,string userID, URole role);
+        Dictionary<string, object> GetMerchantListByPage(string FWBH, int FWSX, string SSQY, string LSFGS, int page, int sixe, string userID, URole role);
         Dictionary<string, object> GetMerchantListHouseDetail(string FWID);
 
         Dictionary<string, object> GetMerchantListShopDetail(string FWID);
-        Dictionary<string, object> GetShopInfoListByPage(string ShopName, int FWSX, string SSQY, string LSFGS, int page, int size);
-        Dictionary<string, object> GetShopInfoList(string ShopName, int FWSX, string SSQY, string LSFGS);
+        Dictionary<string, object> GetShopInfoListByPage(string ShopName, int FWSX, string SSQY, string LSFGS, int page, int size, string userID, URole role);
+        Dictionary<string, object> GetShopInfoList(string ShopName, int FWSX, string SSQY, string LSFGS, string userID, URole role);
         Dictionary<string, object> GetMerchantShopInfoListDetail(string FWID, string CZ_SHID); 
         Dictionary<string, object> GetMerchantShopJiaoFeiList(string FWID, string CZ_SHID); 
     }
